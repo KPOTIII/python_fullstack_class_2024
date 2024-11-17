511a2ad057f9367b065d08e9ce46fc66ccdfbe9b
Nazar <nagrebator2001@gmail.com>
2024-11-17 20:57:28 +0700

    M2_L2_S1

---
+++ lesson_2/solution_1.py
@@ -0,0 +1,8 @@
+cost_string_input: str
+cost_string_output: str
+cost_int: int
+cost_string_input = input ()
+cost_int = int(cost_string_input)
+cost_int = cost_int*2
+cost_string_output = str(cost_int)
+print(cost_string_output)
