80452ef9a2613d7af3aa1348503222d8efc79420
Nazar <nagrebator2001@gmail.com>
2024-12-19 19:01:58 +0700

    M2_L3_S2

---
+++ lesson_3/solution_2.py
@@ -0,0 +1,8 @@
+staff_1 = "Света"
+staff_2 = "Олег"
+staff_3 = "Маша"
+staff_4 = "Паша"
+
+print("В чётные дни работают:", staff_1, staff_2)
+print()
+print("В нечётные дни работают:", staff_3, staff_4)
