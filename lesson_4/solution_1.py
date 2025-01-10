bb6c57b87ca0f7919bcfbd3b304c5ff8f0532f28
Nazar <nagrebator2001@gmail.com>
2025-01-10 20:52:53 +0700

    M2_L4_S1

---
+++ lesson_4/solution_1.py
@@ -0,0 +1,8 @@
+sum_str: str
+sum_float_float: float
+
+sum_str = input()
+sum_float = float (sum_str)
+
+sum_float = round(sum_float)
+print(sum_float)
---
+++ lesson_4/solution_2.py
@@ -0,0 +1,13 @@
+shop_lenght_str : float
+shop_width_str : float
+
+shop_lenght_float : float
+shop_width_float : float
+
+input('Введите длину магазина', shop_lenght_str)
+input('Введите ширину магазина', shop_width_str)
+
+
+shop_area = shop_lenght_float * shop_width_float
+
+print(shop_area)
