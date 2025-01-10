37e9f9f167d79865f0bdb6af2d576ec326e2732a
Nazar <nagrebator2001@gmail.com>
2025-01-10 21:03:56 +0700

    M2_L4_S2

--- lesson_4/solution_2.py
+++ lesson_4/solution_2.py
@@ -1,13 +1,15 @@
-shop_lenght_str : float
-shop_width_str : float
+shop_length_str: str
+shop_width_str: str
 
-shop_lenght_float : float
+shop_length_float : float
 shop_width_float : float
 
-input('Введите длину магазина', shop_lenght_str)
-input('Введите ширину магазина', shop_width_str)
+shop_length_str = input('Введите длину магазина: ')
+shop_width_str = input('Введите ширину магазина: ')
 
+shop_length_float = float(shop_length_str)
+shop_width_float = float(shop_width_str)
 
-shop_area = shop_lenght_float * shop_width_float
+shop_area = shop_length_float * shop_width_float
 
 print(shop_area)
